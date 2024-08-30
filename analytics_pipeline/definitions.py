ea93fb8edd48b9ff7d88f035e275d50313f0ef8c
Santiago Imelio <santiago.imelio@gmail.com>
2024-08-29 22:56:50 -0300

    load airbyte assets

--- analytics_pipeline/definitions.py
+++ analytics_pipeline/definitions.py
@@ -1,9 +1,15 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster_airbyte import AirbyteResource, load_assets_from_airbyte_instance
+from dagster import EnvVar, Definitions
 
-from . import assets
+airbyte_instance = AirbyteResource(
+        host=EnvVar("AIRBYTE_HOST").get_value(),
+        port=EnvVar("AIRBYTE_PORT").get_value(),
+        username=EnvVar("AIRBYTE_USERNAME").get_value(),
+        password=EnvVar("AIRBYTE_PASSWORD").get_value(),
+    )
 
-all_assets = load_assets_from_modules([assets])
+airbyte_assets = load_assets_from_airbyte_instance(airbyte_instance)
 
 defs = Definitions(
-    assets=all_assets,
+    assets=[airbyte_assets]
 )
--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name="analytics_pipeline",
     packages=find_packages(exclude=["analytics_pipeline_tests"]),
     install_requires=[
-        "dagster",
-        "pandas",
-        "pymongo",
-        "psycopg2"
+        "dagster==1.8.1",
+        "dagster-airbyte",
+        "dagster-dbt",
+        "pandas"
     ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
